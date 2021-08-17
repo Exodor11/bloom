5deb8adf5cd27f0d9cdec32154de7f1410e52d9e
Romain Losey <romain.losey@gmail.com>
2021-08-17 14:10:57 +0200

    invitation + mailer setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :user_tags
-  belongs_to :invited_by, class_name: "User", foreign_key: "invited_by_id"
   has_many :posts
   has_many :post_saved
   has_many :post_upvoted
@@ -10,6 +9,6 @@ class User < ApplicationRecord
   has_many :followings, class_name: "Contact", foreign_key: "following_id"
 
 
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
