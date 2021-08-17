fd83bba2f1e575ccdea4ffd6b65f5f66d6f3435f
Marc Leplattenier <marcol.lepl@gmail.com>
2021-08-17 11:56:41 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
   resources :users do
     resources :contacts
     resources :user_tags
+  end
+
+  resources :posts do
+    resources :posts_saved
+    resources :posts_updated
+    resources :post_tags
   end
 end
