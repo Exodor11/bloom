023e29d09d00d56519eb89617160a395fb3233fa
Nathan Cohen <nathco.3011@icloud.com>
2021-08-18 13:56:20 +0200

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#USER SEEDS
-romain = User.new(first_name: 'Romain', last_name: 'Losey', bio: 'Schlague', password: '12345678', password_confirmation: '12345678')
+puts "Destroying previous stuff"
+User.destroy_all
+Post.destroy_all
+puts "Adding new stuff"#USER SEEDS
+romain = User.new(email: "romain@fuckthisshit.com", first_name: 'Romain', last_name: 'Losey', bio: 'Schlague', password: '12345678', password_confirmation: '12345678')
 romain.save!
-marc = User.new(first_name: 'Marc', last_name: 'Leswag', bio: 'Bullish', password: '12345678', password_confirmation: '12345678')
-marc.save!
-
-#POST SEEDS
+marc = User.new(email: "marc@fuckthisshit.com", first_name: 'Marc', last_name: 'Leswag', bio: 'Bullish', password: '12345678', password_confirmation: '12345678')
+marc.save!#POST SEEDS
 post1 = Post.new(title: 'Vaincre le schlague', text: 'pas facile', user: romain)
 post1.save!
 post2 = Post.new(title: "Comment gérer une équipe d'incompétents", text: 'vraiment pas facile', user: marc)
 post2.save!
+
+
