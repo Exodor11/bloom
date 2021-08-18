bdd0a57a85c8d94698b9172fc3a9f10b4970e717
Romain Losey <romain.losey@gmail.com>
2021-08-18 14:43:32 +0200

    tags cold logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,17 @@ class Post < ApplicationRecord
 
   enum category: [ :my_ideas, :ideas_from_others, :freestyle ]
 
+  def self.category_label(label)
+    case label[1]
+      when 0
+        return "My Ideas"
+      when 1
+        return "Others' Ideas"
+      when 2
+        return "Freestyle"
+      end
+  end
+
   # include PgSearch::Model
   # multisearchable against: [:title]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :user_tags
+  has_many :tags, through: :user_tags
   has_many :posts
   has_many :post_saved
   has_many :post_upvoted
