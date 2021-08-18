305cd0100e3aabcbcb6ff311247378d0ae34de18
Marc Leplattenier <marcol.lepl@gmail.com>
2021-08-18 11:53:46 +0200

    tags logic made

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :post_tags
+  has_many :tags, through: :post_tags
   has_many :post_saved
   has_many :post_upvoted
 
+
   enum category: [ :my_ideas, :ideas_from_others, :freestyle ]
 
-  include PgSearch::Model
-  multisearchable against: [:title]
+  # include PgSearch::Model
+  # multisearchable against: [:title]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,12 @@ class User < ApplicationRecord
 
   has_one_attached :photo
 
-  include PgSearch::Model
-  pg_search_scope :search_by_first_name_and_last_name,
-    against: [ :first_name, :last_name],
-    using: {
-      tsearch: { prefix: true }
-    }
+  # include PgSearch::Model
+  # pg_search_scope :search_by_first_name_and_last_name,
+  #   against: [ :first_name, :last_name],
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
 
 
 
