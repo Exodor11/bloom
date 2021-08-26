dc7c0577d3119e45f6b5d4c8c4d266159a367eb0
Romain Losey <romain.losey@gmail.com>
2021-08-26 21:31:31 +0200

    matching members + invited by link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,25 @@ class PagesController < ApplicationController
   def search
     @tags = Tag.all
     @users = User.all
+
+
+    
+    if user_signed_in?
+      @user = current_user
+      @user_tags = @user.tags.map { |tag| tag.name }
+
+      @all_users = User.all
+      @matching_users = []
+
+      @all_users.each do |user|
+        user.tags.each do |tag|
+          @matching_users << user if @user_tags.include?(tag.name) && !@matching_users.include?(user) && user != current_user
+        end
+      end
+      return @matching_users
+    else
+      redirect_to user_session_path
+    end
+
   end
 end
